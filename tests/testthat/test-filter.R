479e352a30575becdbe33e3ae07dda17f07fd8c9
Gene Blanchard <gblanchard4@gmail.com>
2017-02-11 00:33:55 -0600

    more filter tests

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -3,11 +3,15 @@ library("dada2")
 
 # filterAndTrim
 test_that("Output and input files warning", {
-	expect_error(filterAndTrim(sam1F.fastq, sam1F.fastq), "The output and input files must be different.")
+	expect_error(filterAndTrim(sam1F.fastq), "Every input file must have a corresponding output file.")
+})
+
+test_that("Output and input files warning", {
+	expect_error(filterAndTrim(sam1F.fastq, sam1F.fastq), "All output files must be distinct.")
 })
 
 test_that("All inputs have corresponding output", {
-	expect_error(filterAndTrim(c(sam1F.fastq, sam1F.fastq), c(samefile, samefile)), "Every input file must have a corresponding output file.")
+	expect_error(filterAndTrim(c(sam1F.fastq, sam1F.fastq), c(samefile, samefile)), "All output files must be distinct.")
 })
 
 
