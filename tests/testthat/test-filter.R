4f5b8a17ceb7a4c06909c1e4576c513a1d1b8602
Gene Blanchard <gblanchard4@gmail.com>
2017-02-11 00:05:54 -0600

    testthat testing that

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -6,3 +6,8 @@ library("dada2")
 test_that("output and input files warning", {
 	expect_error(fastqFilter(sam1F.fastq, sam1F.fastq), "The output and input files must be different.")
 })
+
+fastqFilter(sam1F.fastq, sam1F_filt)
+test_that("file is output", {
+	expect_that(sam1F_filt, is_a("character"))
+})
