9857f9b4cc9db26219a42b9e953191b13b2aa7e6
Gene Blanchard <gblanchard4@gmail.com>
2017-02-11 00:43:21 -0600

    move data into tests

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -1,6 +1,15 @@
 library("testthat")
 library("dada2")
 
+# Data
+# fastqFilter
+sam1F.fastq <- system.file("extdata", "sam1F.fastq.gz", package="dada2")
+sam1R.fastq <- system.file("extdata", "sam1R.fastq.gz", package="dada2")
+sam2F.fastq <- system.file("extdata", "sam2F.fastq.gz", package="dada2")
+sam2R.fastq <- system.file("extdata", "sam2R.fastq.gz", package="dada2")
+samefile <- tempfile(fileext=".fastq.gz")
+filtFastqs <- c(tempfile(fileext=".fastq.gz"), tempfile(fileext=".fastq.gz"))
+
 # filterAndTrim
 test_that("Output and input files warning", {
 	expect_error(filterAndTrim(sam1F.fastq), "Every input file must have a corresponding output file.")
