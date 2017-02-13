f78fbb2b342eb5ad5bcef0e60a525714667b2dcd
Gene Blanchard <gblanchard4@gmail.com>
2017-02-12 22:23:03 -0600

    fixing reverse reads test

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -8,6 +8,8 @@ sam1R.fastq <- system.file("extdata", "sam1R.fastq.gz", package="dada2")
 sam2F.fastq <- system.file("extdata", "sam2F.fastq.gz", package="dada2")
 sam2R.fastq <- system.file("extdata", "sam2R.fastq.gz", package="dada2")
 samefile <- tempfile(fileext=".fastq.gz")
+out1 <- tempfile(fileext=".fastq.gz")
+out2 <- tempfile(fileext=".fastq.gz")
 filtFastqs <- c(tempfile(fileext=".fastq.gz"), tempfile(fileext=".fastq.gz"))
 
 # filterAndTrim
@@ -23,7 +25,7 @@ test_that("Distinct Output Files", {
 	expect_error(filterAndTrim(c(sam1F.fastq, sam1R.fastq), c(samefile, samefile)), "All output files must be distinct.")
 })
 test_that("Output files for the reverse reads are required.", {
-	expect_error(filterAndTrim(c(sam1F.fastq, sam1R.fastq), c(samefile, samefile), rev=TRUE), "Output files for the reverse reads are required.")
+	expect_error(filterAndTrim(c(sam1F.fastq, sam1R.fastq), c(out1, out2), rev=TRUE), "Output files for the reverse reads are required.")
 })
 
 
