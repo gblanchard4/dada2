80c4aa7100c37858ea7a9c937b80615a81bf35c7
Gene Blanchard <gblanchard4@gmail.com>
2017-02-12 22:10:09 -0600

    added some more test for filterAndTrim

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -15,23 +15,25 @@ test_that("Output and input files warning", {
 	expect_error(filterAndTrim(sam1F.fastq), "Every input file must have a corresponding output file.")
 })
 
-test_that("Output and input files warning", {
-	expect_error(filterAndTrim(sam1F.fastq, sam1F.fastq), "All output files must be distinct.")
+test_that("Output files must be distinct from input files", {
+	expect_error(filterAndTrim(sam1F.fastq, sam1F.fastq), "Output files must be distinct from the input files.")
 })
 
-test_that("All inputs have corresponding output", {
-	expect_error(filterAndTrim(c(sam1F.fastq, sam1F.fastq), c(samefile, samefile)), "All output files must be distinct.")
+test_that("Distinct Output Files", {
+	expect_error(filterAndTrim(c(sam1F.fastq, sam1R.fastq), c(samefile, samefile)), "All output files must be distinct.")
+})
+test_that("Output files for the reverse reads are required.", {
+	expect_error(filterAndTrim(c(sam1F.fastq, sam1R.fastq), c(samefile, samefile), rev=TRUE), "Output files for the reverse reads are required.")
 })
 
 
 
-
-# fastqFilter
-test_that("output and input files warning", {
-	expect_error(fastqFilter(sam1F.fastq, sam1F.fastq), "The output and input files must be different.")
-})
-
-fastqFilter(sam1F.fastq, sam1F_filt)
-test_that("file is output", {
-	expect_that(sam1F_filt, is_a("character"))
-})
+# # fastqFilter
+# test_that("output and input files warning", {
+# 	expect_error(fastqFilter(sam1F.fastq, sam1F.fastq), "The output and input files must be different.")
+# })
+# 
+# fastqFilter(sam1F.fastq, sam1F_filt)
+# test_that("file is output", {
+# 	expect_that(sam1F_filt, is_a("character"))
+# })
